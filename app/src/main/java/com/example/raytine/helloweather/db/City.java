a3b1e02c74291c85f8a9565dd80f225438a30208
Rulemylife <18502647875@163.com>
2018-01-08 22:15:23 +0800

    first commit

---
+++ app/src/main/java/com/example/raytine/helloweather/db/City.java
@@ -0,0 +1,47 @@
+package com.example.raytine.helloweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Raytine on 2018/1/8.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
